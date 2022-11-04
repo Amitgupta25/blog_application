e479ac17bed2c4a0eb77ef709441b41e4603c8c5
Amit Gupta <amgupta@deqode.com>
2022-11-04 20:15:10 +0530

    Added a validation spec

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  subject{described_class.new}
+  
+    it "Title must be present" do
+      subject.title = ""
+      expect(subject).not_to be_valid
+    end
+
+    it "Body must be present" do
+      subject.body = ""
+      expect(subject).not_to be_valid
+    end
+end
+
