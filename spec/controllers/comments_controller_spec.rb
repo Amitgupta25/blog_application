8504b0b66dc5224674543de4ffb2689eb6a90975
Amit Gupta <amgupta@deqode.com>
2022-11-09 13:09:24 +0530

    Added all the controller specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe CommentsController, :type => :controller do
-  # before(:all) {comment = FactoryBot.create (:comment)}
+  before(:all) {@article = FactoryBot.create (:article)}
+
+  it "has a 200 status code" do
+    get :index, params: {article_id: @article.id}
+    expect(response.status).to eq(200)
+  end
 
   it "responds to custom format" do
-    post :create, :params => { :comment => { article_id: 1, commenter: "commenter_name", body: "comment_body", status: "AnyOfTheThree" }, :format => :json }
+    post :create, params:  { article_id: @article.id, comment: { commenter: "commenter_name", body: "comment_body", status: "AnyOfTheThree" }, :format => :json}
     expect(response.content_type).to eq "application/json; charset=utf-8"
   end
+
+  it "destroy the article and display the message" do 
+    comment = FactoryBot.create (:comment)
+    expect{ delete :destroy, :id => comment.id}
+  end
+
 end
